23dd28301e6f38f6e4c9340c9e256c4a537bc90c
lomyal <wangshijun1995@gmail.com>
2017-03-08 08:34:50 +0800

    improve

--- pure_cnn.py
+++ pure_cnn.py
@@ -20,6 +20,9 @@ mnist = input_data.read_data_sets(
 import tensorflow as tf
 sess = tf.InteractiveSession()
 
+import utils.io as io
+
+
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
--- utils/io.py
+++ utils/io.py
@@ -16,10 +16,10 @@ Date:  2017/03/01 22:39:00
 import h5py
 
 
-def read_image_file(file_path):
-    return h5py.File(file_path, 'r')
+def read_image_file(file_name, file_path='/media/blade/road_hackers/training_images/'):
+    return h5py.File(file_path + file_name, 'r')
 
 
-def read_attribute_file(file_path):
-    attribute_object = h5py.File(file_path, 'r')
+def read_attribute_file(file_name, file_path='/media/blade/road_hackers/training_attribute/'):
+    attribute_object = h5py.File(file_path + file_name, 'r')
     return attribute_object['attrs']
