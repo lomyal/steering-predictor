00a4f46b77ad3e3fa58cc7de2d55650cc1a6b547
wangshijun <wangshijun03@baidu.com>
2017-03-08 18:57:34 +0800

    adjust to rdhckrs data

--- pure_cnn.py
+++ pure_cnn.py
@@ -12,47 +12,63 @@ Authors: Wang Shijun
 Date:    2017/03/07 23:00:00
 """
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets(
-    '/home/lomyal/coding/deep/tf-1.0.0-gpu-playground/MNIST_data', 
-    one_hot=True)
-
 import tensorflow as tf
-sess = tf.InteractiveSession()
 
-import utils.io as io
+import utils.io
+
+
+# from tensorflow.examples.tutorials.mnist import input_data
+# mnist = input_data.read_data_sets(
+#     'MNIST_data',
+#     one_hot=True)
+
+rdhckrs_train = utils.io.IO('554.h5')
+rdhckrs_test = utils.io.IO('554.h5')
 
 
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
+
 def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
+
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
 
 def max_pool_2x2(x):
     return tf.nn.max_pool(
         x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-x = tf.placeholder(tf.float32, shape=[None, 784])
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+sess = tf.InteractiveSession()
+
+# 三色（RGB）图像，大小 320x320
+x = tf.placeholder(tf.float32, shape=[None, 320, 320, 3])
+# y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+# 0.125 秒后的曲率
+y_ = tf.placeholder(tf.float32, shape=[None, 1])
 
 # == First Convolutional Layer == #
 
 # The convolution will compute 32 features for each 5x5 patch.
 # The first two dimensions are the patch size, the next is the number
 # of input channels, and the last is the number of output channels.
-W_conv1 = weight_variable([5, 5, 1, 32])
+# 三色
+W_conv1 = weight_variable([5, 5, 3, 32])
 b_conv1 = bias_variable([32])
 
 # To apply the layer, we first reshape x to a 4d tensor, with the
 # second and third dimensions corresponding to image width and height,
 # and the final dimension corresponding to the number of color channels.
-x_image = tf.reshape(x, [-1,28,28,1])
+
+# x_image = tf.reshape(x, [-1, 320, 320, 3])
+x_image = x
 
 # We then convolve x_image with the weight tensor, add the bias, apply
 # the ReLU function, and finally max pool. The max_pool_2x2 method will
@@ -75,9 +91,9 @@ h_pool2 = max_pool_2x2(h_conv2)
 # fully-connected layer with 1024 neurons to allow processing on the
 # entire image. We reshape the tensor from the pooling layer into a batch
 # of vectors, multiply by a weight matrix, add a bias, and apply a ReLU.
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
+W_fc1 = weight_variable([80 * 80 * 64, 1024])
 b_fc1 = bias_variable([1024])
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 80 * 80 * 64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 # To reduce overfitting, we will apply DROPOUT before the readout layer.
@@ -93,33 +109,33 @@ h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 # Finally, we add a layer, just like for the one layer softmax regression
 # above.
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
+W_fc2 = weight_variable([1024, 1])
+b_fc2 = bias_variable([1])
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
-cross_entropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(
+mse_loss = tf.losses.mean_squared_error(
         labels=y_,
-        logits=y_conv))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        predictions=y_conv)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(mse_loss)
+# correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 sess.run(tf.global_variables_initializer())
 for i in range(20000):
-    batch = mnist.train.next_batch(50)
-    if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
-            x:batch[0], 
-            y_: batch[1], 
-            keep_prob: 1.0,})
-        print("step %d, training accuracy %g"%(i, train_accuracy))
+    batch = rdhckrs_train.next_batch(50)
+    if i % 1 == 0:
+        print(i)
+        train_loss = mse_loss.eval(feed_dict={
+            x: batch[0],
+            y_: batch[1],
+            keep_prob: 1.0})
+        print("step %d, training loss %g" % (i, train_loss))
     train_step.run(feed_dict={
         x: batch[0], 
         y_: batch[1], 
-        keep_prob: 0.5,})
+        keep_prob: 0.5})
 
-print("test accuracy %g"%accuracy.eval(feed_dict={
-    x: mnist.test.images, 
-    y_: mnist.test.labels, 
-    keep_prob: 1.0,}))
+# print("test accuracy %g" % accuracy.eval(feed_dict={
+#     x: rdhckrs_test.images,
+#     y_: rdhckrs_test.labels,
+#     keep_prob: 1.0}))
--- utils/io.py
+++ utils/io.py
@@ -14,12 +14,52 @@ Date:  2017/03/01 22:39:00
 """
 
 import h5py
+import numpy as np
 
 
-def read_image_file(file_name, file_path='/media/blade/road_hackers/training_images/'):
-    return h5py.File(file_path + file_name, 'r')
+class IO(object):
 
+    def __init__(self, file_name):
+        self.training_image_path = '/media/blade/road_hackers/training_images/'
+        self.training_attribute_path = '/media/blade/road_hackers/training_attribute/'
+        self.file_name = file_name
+        self.training_images = self.read_image_file(self.file_name)
+        self.training_attribute = self.read_attribute_file(self.file_name)
 
-def read_attribute_file(file_name, file_path='/media/blade/road_hackers/training_attribute/'):
-    attribute_object = h5py.File(file_path + file_name, 'r')
-    return attribute_object['attrs']
+        self.returned_batch_count = 0
+        self.training_attribute_count = 0
+
+        # self._pre_process_data()
+
+    def read_image_file(self, file_name):
+        return h5py.File(self.training_image_path + file_name, 'r')
+
+    def read_attribute_file(self, file_name):
+        attribute_object = h5py.File(self.training_attribute_path + file_name, 'r')
+        return attribute_object['attrs']
+
+    def next_batch(self, batch_size):
+        """
+        返回下一块数据
+        :param batch_size:
+        :return:
+        """
+        images = np.ndarray(shape=(batch_size, 320, 320, 3,))
+        labels = np.ndarray(shape=(batch_size, 1,))
+        data_count = 0
+        while data_count < batch_size:
+            label_data = self.training_attribute[self.training_attribute_count]
+            utc_time = "{0:.3f}".format(label_data[0])
+            if utc_time in self.training_images:
+                images[data_count] = self.training_images[utc_time]
+                labels[data_count] = label_data[1]
+                data_count += 1
+                self.returned_batch_count += 1
+        return [images, labels]
+
+    # def _pre_process_data(self):
+    #     """
+    #     将原始数据组装成
+    #     :return:
+    #     """
+    #     pass
