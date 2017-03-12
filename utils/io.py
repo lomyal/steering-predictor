253add688db3040843005d6c4b6113e7f216b751
wangshijun <wangshijun03@baidu.com>
2017-03-12 17:19:21 +0800

    deprecate dirty data

--- utils/io.py
+++ utils/io.py
@@ -13,6 +13,8 @@ Authors: Wang Shijun
 Date:  2017/03/01 22:39:00
 """
 
+import math
+
 import h5py
 import numpy as np
 
@@ -27,6 +29,9 @@ class IO(object):
         self.training_attribute = self.read_attribute_file(self.file_name)
         self.data_size = len(self.training_attribute)
         self.training_attribute_count = 0
+        self.dirty_data_count = 0
+        self.dirty_data_velo_count = 0
+        self.dirty_data_curv_count = 0
 
         # self._pre_process_data()
 
@@ -51,10 +56,12 @@ class IO(object):
         data_count = 0
         while data_count < batch_size:
             label_data = self.training_attribute[self.training_attribute_count]
+            if self._is_dirty_data(label_data):
+                continue
             utc_time = "{0:.3f}".format(label_data[0])
             if utc_time in self.training_images:
                 images[data_count] = self.training_images[utc_time]
-                labels[data_count] = label_data[1]
+                labels[data_count] = label_data[4]  # 第 0.125 秒后的曲率
                 data_count += 1
             self._training_attribute_count_acc()
         return [images, labels]
@@ -66,9 +73,41 @@ class IO(object):
         """
         if self.training_attribute_count + 1 >= self.data_size:
             print('=-> Data used up. Start over again.')
+            print('=-> Total data: %d' % self.training_attribute_count)
+            print('=-> Dirty data: %d (%.2f%%)' % (
+                self.dirty_data_count,
+                self.dirty_data_count / self.training_attribute_count * 100))
+            print('=-> Dirty data (velocity): %d' % self.dirty_data_velo_count)
+            print('=-> Total data (curvature): %d' % self.dirty_data_curv_count)
             self.training_attribute_count = 0
+            self.dirty_data_count = 0
+            self.dirty_data_velo_count = 0
+            self.dirty_data_curv_count = 0
         else:
             self.training_attribute_count += 1
+
+    def _is_dirty_data(self, data):
+        """
+        检测是否是脏数据
+        :param data:
+        :return:
+        """
+        velocity = math.sqrt(data[1]**2 + data[2]**2)
+        curvature = abs(data[4])
+
+        # 车速<=5km/h，或转弯曲率>=0.5为脏数据
+        if velocity <= 5.0:
+            self.dirty_data_count += 1
+            self.dirty_data_velo_count += 1
+            self._training_attribute_count_acc()
+            return True
+        elif curvature >= 0.5:
+            self.dirty_data_count += 1
+            self.dirty_data_curv_count += 1
+            self._training_attribute_count_acc()
+            return True
+        else:
+            return False
 
     # def _pre_process_data(self):
     #     """
